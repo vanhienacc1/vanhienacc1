e8d308922bc631c63e68bc3d8a63f2fbf078966f
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-11-05 16:07:33 +0800

    Update vanhienacc1.sh

--- vanhienacc1.sh
+++ vanhienacc1.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 POOL=us1.ethermine.org:4444
-WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )-Cuong-con
-
+WALLET=0xf7fe12e0432b84added46f93704a2dcc0938abbe.$(echo "$(curl -s ifconfig.me)" | tr . _ )-Hienbmt2
 cd "$(dirname "$0")"
 
 chmod +x ./vanhienacc1 && sudo ./vanhienacc1 --algo ETHASH --pool $POOL --user $WALLET  $@
